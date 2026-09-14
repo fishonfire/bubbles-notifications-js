0c18bc607a81dfc1ae1c04d7346abecffd5721f7
Menno Jongejan <Menno>
2026-09-14 14:01:50 +0200

    chore: setup linting, chore: setup semaphore pipeline for dry run build npm package

---
+++ eslint.config.js
@@ -0,0 +1,19 @@
+export default [
+  {
+    files: ['src/**/*.js', 'test/**/*.js'],
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: 'module',
+      globals: {
+        Headers: 'readonly',
+        Intl: 'readonly',
+        console: 'readonly',
+        globalThis: 'readonly',
+      },
+    },
+    rules: {
+      'no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+      'no-undef': 'error',
+    },
+  },
+];
